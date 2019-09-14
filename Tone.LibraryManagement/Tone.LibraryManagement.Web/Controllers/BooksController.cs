92f096989e548e74decc57dfaa5cd82b2deb5e27
Kevin Kraus <kkraus@microsoft.com>
2019-09-14 12:05:22 -0600

    added MVC project

---
+++ Tone.LibraryManagement/Tone.LibraryManagement.Web/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tone.LibraryManagement.Web.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
