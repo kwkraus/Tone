f89d4805d37f00aeffa1bdf62abe2fd430ca9051
Kevin Kraus <kkraus@microsoft.com>
2019-09-14 11:59:04 -0600

    added rest of CRUD methods and made async.

--- Tone.LibraryManagement/Tone.LibraryManagement.WebApi/Controllers/BooksController.cs
+++ Tone.LibraryManagement/Tone.LibraryManagement.WebApi/Controllers/BooksController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Tone.LibraryManagement.Data.Contexts;
 using Tone.LibraryManagement.Data.Entities;
 
@@ -26,28 +28,40 @@ namespace Tone.LibraryManagement.WebApi.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<Book> Get(int id)
+        public async Task<ActionResult<Book>> Get(int id)
         {
-            return _context.Books.Find(id);
+            return await _context.Books.FindAsync(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] Book value)
+        public async Task<ActionResult> Post([FromBody] Book value)
         {
+            //the Book passed in will be automatically validated and 400 (BadRequest) returned.
+            //if successful, return 204 (NoContent)
+            await _context.Books.AddAsync(value);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Book value)
+        public async Task<ActionResult> Put(int id, [FromBody] Book value)
         {
+            _context.Entry(await _context.Books.FirstOrDefaultAsync(x => x.BookId == id)).CurrentValues.SetValues(value);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            //must find the book to delete before deleting it from db
+            var bookToDelete = await _context.Books.FindAsync(id);
+            _context.Remove(bookToDelete);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
-
     }
 }
